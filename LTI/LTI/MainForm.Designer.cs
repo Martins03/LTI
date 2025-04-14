cdeb64e320bf19fd79828bba799f3cd30530a1ce
tomaspinote <2221459@my.ipleiria.pt>
2025-04-14 19:44:00 +0100

    o que fiz
    o Listar todas as interfaces do dispositivo
    o Listar apenas as interfaces wireless
    o Listar/criar/editar/apagar interfaces bridge e respetivas portas associadas
    feito

--- LTI/LTI/MainForm.Designer.cs
+++ LTI/LTI/MainForm.Designer.cs
@@ -12,10 +12,15 @@
         private System.Windows.Forms.DataGridView dgvInterfaces;
         private System.Windows.Forms.DataGridView dgvWireless;
         private System.Windows.Forms.DataGridView dgvBridges;
+        private System.Windows.Forms.DataGridView dgvBridgePorts;
 
         private System.Windows.Forms.Button btnGetInterfaces;
         private System.Windows.Forms.Button btnGetWireless;
         private System.Windows.Forms.Button btnGetBridges;
+        private System.Windows.Forms.Button btnAddBridge;
+        private System.Windows.Forms.Button btnDeleteBridge;
+        private System.Windows.Forms.Button btnEditBridge;
+        private System.Windows.Forms.Button btnGetBridgePorts;
 
         protected override void Dispose(bool disposing)
         {
@@ -34,10 +39,15 @@
             this.dgvInterfaces = new System.Windows.Forms.DataGridView();
             this.dgvWireless = new System.Windows.Forms.DataGridView();
             this.dgvBridges = new System.Windows.Forms.DataGridView();
+            this.dgvBridgePorts = new System.Windows.Forms.DataGridView();
 
             this.btnGetInterfaces = new System.Windows.Forms.Button();
             this.btnGetWireless = new System.Windows.Forms.Button();
             this.btnGetBridges = new System.Windows.Forms.Button();
+            this.btnAddBridge = new System.Windows.Forms.Button();
+            this.btnDeleteBridge = new System.Windows.Forms.Button();
+            this.btnEditBridge = new System.Windows.Forms.Button();
+            this.btnGetBridgePorts = new System.Windows.Forms.Button();
 
             this.tabControl.SuspendLayout();
             this.tabInterfaces.SuspendLayout();
@@ -46,11 +56,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.dgvInterfaces)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvWireless)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvBridges)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBridgePorts)).BeginInit();
             this.SuspendLayout();
 
-            // 
             // tabControl
-            // 
             this.tabControl.Controls.Add(this.tabInterfaces);
             this.tabControl.Controls.Add(this.tabWireless);
             this.tabControl.Controls.Add(this.tabBridges);
@@ -58,81 +67,98 @@
             this.tabControl.Size = new System.Drawing.Size(760, 537);
             this.tabControl.TabIndex = 0;
 
-            // 
             // tabInterfaces
-            // 
             this.tabInterfaces.Controls.Add(this.btnGetInterfaces);
             this.tabInterfaces.Controls.Add(this.dgvInterfaces);
             this.tabInterfaces.Text = "Interfaces";
 
-            // 
             // tabWireless
-            // 
             this.tabWireless.Controls.Add(this.btnGetWireless);
             this.tabWireless.Controls.Add(this.dgvWireless);
             this.tabWireless.Text = "Wireless";
 
-            // 
             // tabBridges
-            // 
             this.tabBridges.Controls.Add(this.btnGetBridges);
+            this.tabBridges.Controls.Add(this.btnAddBridge);
+            this.tabBridges.Controls.Add(this.btnDeleteBridge);
+            this.tabBridges.Controls.Add(this.btnEditBridge);
+            this.tabBridges.Controls.Add(this.btnGetBridgePorts);
             this.tabBridges.Controls.Add(this.dgvBridges);
+            this.tabBridges.Controls.Add(this.dgvBridgePorts);
             this.tabBridges.Text = "Bridges";
 
-            // 
             // dgvInterfaces
-            // 
             this.dgvInterfaces.Location = new System.Drawing.Point(6, 50);
             this.dgvInterfaces.Size = new System.Drawing.Size(740, 420);
             this.dgvInterfaces.ColumnCount = 2;
             this.dgvInterfaces.Columns[0].Name = "Nome";
             this.dgvInterfaces.Columns[1].Name = "Tipo";
 
-            // 
             // dgvWireless
-            // 
             this.dgvWireless.Location = new System.Drawing.Point(6, 50);
             this.dgvWireless.Size = new System.Drawing.Size(740, 420);
             this.dgvWireless.ColumnCount = 2;
             this.dgvWireless.Columns[0].Name = "Nome";
             this.dgvWireless.Columns[1].Name = "Tipo";
 
-            // 
             // dgvBridges
-            // 
             this.dgvBridges.Location = new System.Drawing.Point(6, 50);
-            this.dgvBridges.Size = new System.Drawing.Size(740, 420);
-            this.dgvBridges.ColumnCount = 2;
-            this.dgvBridges.Columns[0].Name = "Nome";
-            this.dgvBridges.Columns[1].Name = "Tipo";
+            this.dgvBridges.Size = new System.Drawing.Size(740, 200);
+            this.dgvBridges.ColumnCount = 3;
+            this.dgvBridges.Columns[0].Name = "ID";
+            this.dgvBridges.Columns[1].Name = "Nome";
+            this.dgvBridges.Columns[2].Name = "Tipo";
 
-            // 
+            // dgvBridgePorts
+            this.dgvBridgePorts.Location = new System.Drawing.Point(6, 260);
+            this.dgvBridgePorts.Size = new System.Drawing.Size(740, 210);
+            this.dgvBridgePorts.ColumnCount = 2;
+            this.dgvBridgePorts.Columns[0].Name = "Bridge";
+            this.dgvBridgePorts.Columns[1].Name = "Interface";
+
             // btnGetInterfaces
-            // 
             this.btnGetInterfaces.Location = new System.Drawing.Point(6, 10);
             this.btnGetInterfaces.Size = new System.Drawing.Size(150, 30);
             this.btnGetInterfaces.Text = "Listar Interfaces";
             this.btnGetInterfaces.Click += new System.EventHandler(this.btnGetInterfaces_Click);
 
-            // 
             // btnGetWireless
-            // 
             this.btnGetWireless.Location = new System.Drawing.Point(6, 10);
             this.btnGetWireless.Size = new System.Drawing.Size(150, 30);
             this.btnGetWireless.Text = "Listar Wireless";
             this.btnGetWireless.Click += new System.EventHandler(this.btnGetWireless_Click);
 
-            // 
             // btnGetBridges
-            // 
             this.btnGetBridges.Location = new System.Drawing.Point(6, 10);
             this.btnGetBridges.Size = new System.Drawing.Size(150, 30);
             this.btnGetBridges.Text = "Listar Bridges";
             this.btnGetBridges.Click += new System.EventHandler(this.btnGetBridges_Click);
 
-            // 
+            // btnAddBridge
+            this.btnAddBridge.Location = new System.Drawing.Point(162, 10);
+            this.btnAddBridge.Size = new System.Drawing.Size(150, 30);
+            this.btnAddBridge.Text = "Criar Bridge";
+            this.btnAddBridge.Click += new System.EventHandler(this.btnAddBridge_Click);
+
+            // btnDeleteBridge
+            this.btnDeleteBridge.Location = new System.Drawing.Point(318, 10);
+            this.btnDeleteBridge.Size = new System.Drawing.Size(150, 30);
+            this.btnDeleteBridge.Text = "Apagar Bridge";
+            this.btnDeleteBridge.Click += new System.EventHandler(this.btnDeleteBridge_Click);
+
+            // btnEditBridge
+            this.btnEditBridge.Location = new System.Drawing.Point(474, 10);
+            this.btnEditBridge.Size = new System.Drawing.Size(150, 30);
+            this.btnEditBridge.Text = "Editar Bridge";
+            this.btnEditBridge.Click += new System.EventHandler(this.btnEditBridge_Click);
+
+            // btnGetBridgePorts
+            this.btnGetBridgePorts.Location = new System.Drawing.Point(630, 10);
+            this.btnGetBridgePorts.Size = new System.Drawing.Size(150, 30);
+            this.btnGetBridgePorts.Text = "Listar Portas";
+            this.btnGetBridgePorts.Click += new System.EventHandler(this.btnGetBridgePorts_Click);
+
             // MainForm
-            // 
             this.ClientSize = new System.Drawing.Size(784, 561);
             this.Controls.Add(this.tabControl);
             this.Name = "MainForm";
@@ -145,6 +171,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dgvInterfaces)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvWireless)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvBridges)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBridgePorts)).EndInit();
             this.ResumeLayout(false);
         }
     }
--- LTI/LTI/MainForm.cs
+++ LTI/LTI/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// MainForm.cs
+using System;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -33,6 +34,155 @@ namespace MikrotikManagerApp
             await LoadData("interface/bridge", dgvBridges);
         }
 
+        private async void btnAddBridge_Click(object sender, EventArgs e)
+        {
+            string name = Prompt.ShowDialog("Digite o nome da bridge (apenas letras, números e hífens):", "Criar Bridge");
+            if (string.IsNullOrEmpty(name))
+            {
+                MessageBox.Show("O nome da bridge não pode ser vazio.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!System.Text.RegularExpressions.Regex.IsMatch(name, @"^[a-zA-Z0-9-]+$"))
+            {
+                MessageBox.Show("O nome da bridge deve conter apenas letras, números e hífens.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                var data = new { name = name, mtu = "1500" };
+                var json = JsonSerializer.Serialize(data);
+                var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+                var response = await _httpClient.PostAsync($"{_baseUrl}/rest/interface/bridge/add", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show($"Bridge '{name}' criada com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    await LoadData("interface/bridge", dgvBridges);
+                }
+                else
+                {
+                    var error = await response.Content.ReadAsStringAsync();
+                    MessageBox.Show($"Erro ao criar bridge: {response.StatusCode}\n{error}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao criar bridge: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void btnDeleteBridge_Click(object sender, EventArgs e)
+        {
+            if (dgvBridges.SelectedRows.Count > 0)
+            {
+                string id = dgvBridges.SelectedRows[0].Cells[0].Value.ToString();
+                string name = dgvBridges.SelectedRows[0].Cells[1].Value.ToString();
+
+                // Impede apagar a bridge1
+                if (name.Equals("bridge1", StringComparison.OrdinalIgnoreCase))
+                {
+                    MessageBox.Show("A bridge 'bridge1' não pode ser apagada.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                try
+                {
+                    var response = await _httpClient.DeleteAsync($"{_baseUrl}/rest/interface/bridge/{id}");
+                    MessageBox.Show(response.IsSuccessStatusCode ? "Apagado com sucesso!" : "Erro ao apagar.");
+                    await LoadData("interface/bridge", dgvBridges);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro ao apagar bridge: {ex.Message}", "Erro");
+                }
+            }
+        }
+
+
+        private async void btnEditBridge_Click(object sender, EventArgs e)
+        {
+            if (dgvBridges.SelectedRows.Count > 0)
+            {
+                string id = dgvBridges.SelectedRows[0].Cells[0].Value.ToString();
+                string currentName = dgvBridges.SelectedRows[0].Cells[1].Value.ToString();
+
+                if (currentName.Equals("bridge1", StringComparison.OrdinalIgnoreCase))
+                {
+                    MessageBox.Show("A bridge 'bridge1' não pode ser editada.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                string newName = Prompt.ShowDialog("Novo nome da bridge:", "Editar Bridge");
+
+                if (!string.IsNullOrWhiteSpace(newName))
+                {
+                    try
+                    {
+                        var json = JsonSerializer.Serialize(new { name = newName });
+                        var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+                        var request = new HttpRequestMessage(new HttpMethod("PATCH"), $"{_baseUrl}/rest/interface/bridge/{id}")
+                        {
+                            Content = content
+                        };
+
+                        var response = await _httpClient.SendAsync(request);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            MessageBox.Show("Bridge atualizada com sucesso!", "Sucesso");
+                            await LoadData("interface/bridge", dgvBridges);
+                        }
+                        else
+                        {
+                            string error = await response.Content.ReadAsStringAsync();
+                            MessageBox.Show($"Erro ao editar bridge: {response.StatusCode}\n{error}", "Erro");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Erro: {ex.Message}", "Exceção");
+                    }
+                }
+            }
+        }
+        private async void btnGetBridgePorts_Click(object sender, EventArgs e)
+        {
+            await LoadBridgePorts();
+        }
+
+
+        private async Task LoadBridgePorts()
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync($"{_baseUrl}/rest/interface/bridge/port");
+                response.EnsureSuccessStatusCode();
+
+                var json = await response.Content.ReadAsStringAsync();
+                var doc = JsonDocument.Parse(json);
+
+                dgvBridgePorts.Rows.Clear();
+
+                foreach (var item in doc.RootElement.EnumerateArray())
+                {
+                    string bridge = item.GetProperty("bridge").GetString();
+                    string iface = item.GetProperty("interface").GetString();
+                    dgvBridgePorts.Rows.Add(bridge, iface);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao carregar portas: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+
         private async Task LoadData(string endpoint, DataGridView grid)
         {
             try
@@ -47,15 +197,39 @@ namespace MikrotikManagerApp
 
                 foreach (var item in doc.RootElement.EnumerateArray())
                 {
+                    string id = item.GetProperty(".id").GetString();
                     string name = item.GetProperty("name").GetString();
                     string type = item.TryGetProperty("type", out var t) ? t.GetString() : "";
-                    grid.Rows.Add(name, type);
+                    grid.Rows.Add(id, name, type);
                 }
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"Erro ao carregar dados: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+    }
+
+    public static class Prompt
+    {
+        public static string ShowDialog(string text, string caption)
+        {
+            Form prompt = new Form()
+            {
+                Width = 300,
+                Height = 150,
+                Text = caption
+            };
+            Label lbl = new Label() { Left = 10, Top = 20, Text = text };
+            TextBox inputBox = new TextBox() { Left = 10, Top = 50, Width = 250 };
+            Button confirmation = new Button() { Text = "OK", Left = 10, Width = 250, Top = 80 };
+            confirmation.Click += (sender, e) => { prompt.DialogResult = DialogResult.OK; prompt.Close(); };
+            prompt.Controls.Add(lbl);
+            prompt.Controls.Add(inputBox);
+            prompt.Controls.Add(confirmation);
+            prompt.AcceptButton = confirmation;
+
+            return prompt.ShowDialog() == DialogResult.OK ? inputBox.Text : "";
         }
     }
 }
